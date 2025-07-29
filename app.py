39db6aeab399b6605796f8f4c5a424d3068505bf
krishnabalajiwork <krishnabalajiwork@gmail.com>
2025-07-29 12:11:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,19 +49,12 @@ st.markdown("""
 }
 
 @keyframes bounce {
-    0%, 20%, 50%, 80%, 100% {
-        transform: translateY(0);
-    }
-    40% {
-        transform: translateY(-10px);
-    }
-    60% {
-        transform: translateY(-5px);
-    }
+    0%,20%,50%,80%,100% {transform: translateY(0);}
+    40% {transform: translateY(-10px);}
+    60% {transform: translateY(-5px);}
 }
 </style>
 """, unsafe_allow_html=True)
-
 
 # Page configuration
 st.set_page_config(
